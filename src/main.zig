cb61f458408a6b61eb6daa5594ffe9c2a03b053f
Sigmanificient <edhyjox@gmail.com>
2025-07-16 05:42:28 +0200

    Allow player override via CLI

--- src/main.zig
+++ src/main.zig
@@ -10,6 +10,38 @@ const config = @import("config.zig");
 const server = @import("server.zig");
 const utils = @import("utils.zig");
 
+pub const PlayerOverrides = struct {
+    game_player1: ?[]const u8,
+    game_player2: ?[]const u8,
+};
+
+pub fn player_overrides(allocator: std.mem.Allocator) !PlayerOverrides {
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    var game_player1: ?[]const u8 = null;
+    var game_player2: ?[]const u8 = null;
+
+    var i: usize = 1;
+    while (i < args.len) : (i += 1) {
+        const arg = args[i];
+        if (std.mem.eql(u8, arg, "-p1")) {
+            if (i + 1 >= args.len) return error.InvalidArguments;
+            game_player1 = try allocator.dupe(u8, args[i + 1]);
+            i += 1;
+        } else if (std.mem.eql(u8, arg, "-p2")) {
+            if (i + 1 >= args.len) return error.InvalidArguments;
+            game_player2 = try allocator.dupe(u8, args[i + 1]);
+            i += 1;
+        }
+    }
+
+    return PlayerOverrides{
+        .game_player1 = game_player1,
+        .game_player2 = game_player2,
+    };
+}
+
 pub fn main() !void {
     const start_time = std.time.milliTimestamp();
 
@@ -28,8 +60,12 @@ pub fn main() !void {
     });
     defer logz.deinit();
 
-    const conf = try config.parse("config.ini");
+    var conf = try config.parse("config.ini");
     defer config.deinit_config(config.Config, &conf);
+
+    const pl = try player_overrides(utils.allocator);
+    if (pl.game_player1) |p1| conf.game_player1 = p1;
+    if (pl.game_player2) |p2| conf.game_player2 = p2;
 
     try logz.setup(utils.allocator, .{
         .level = conf.log_level,
